035471f81ebedb972a34503f43f37ed6e1aeb1ee
ibiensedev <ibiensedev@gmail.com>
2023-06-06 19:00:13 +0200

    importar proyecto de remoto github y hacer cambio

--- ejemplo_git/src/main/java/ejemplo_git/Principal.java
+++ ejemplo_git/src/main/java/ejemplo_git/Principal.java
@@ -4,7 +4,7 @@ public class Principal {
 
 	public static void main(String[] args) {
 		System.out.println("Este es mi primer proyecto con Git.");
-
+		System.out.println("Segunda modificacion en proyecto de Git nuevo.");
 	}
 
 }
